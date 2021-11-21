f843fe1fb0ec7697ecba9332f04e1e52e8f1fb30
Tri Le <trilq1402@gmail.com>
2021-11-21 15:45:00 -0800

    delete unnesscesary files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,8 @@
-import firstObjectReducer from "./firstObject";
-import secondObjectReducer from "./secondObject";
 import { combineReducers } from "redux";
 import stockReducer from "./stocks";
 
 const rootReducer = combineReducers({
-    firstObject: firstObjectReducer,
-    secondObject: secondObjectReducer,
-    stock:stockReducer,
+    stock: stockReducer,
 });
 
 export default rootReducer;
