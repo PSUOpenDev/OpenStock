a257a284a6e77f6c02d7e67aa149680a927d6abe
Phuoc T. Nguyen <55012667+jesuispius@users.noreply.github.com>
2021-12-02 11:00:23 -0800

    add a backup image for newspaper and switch to another news API

--- src/components/Common/APIUtils/News/ApiParameter.js
+++ src/components/Common/APIUtils/News/ApiParameter.js
@@ -1,1 +1,2 @@
-export const API_NEWS_URL = "https://newsapi.org/v2/everything?"
+//export const API_NEWS_URL = "https://newsapi.org/v2/everything?"
+export const API_NEWS_URL = "http://api.mediastack.com/v1/news"
