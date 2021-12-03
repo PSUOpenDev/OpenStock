9d411572b229ea2af9f947cba0ad0150900a8836
Phuoc T. Nguyen <55012667+jesuispius@users.noreply.github.com>
2021-12-02 23:38:28 -0800

    temporarily remove the small chart

--- src/components/Common/PriceCard/index.jsx
+++ src/components/Common/PriceCard/index.jsx
@@ -15,7 +15,9 @@ const down = { color: "red" };
 const badge_up = { bg: "success" };
 const badge_down = { bg: "danger" };
 
-
+PriceCard.propTypes = {
+    stockSymbol: PropTypes.object.isRequired,
+};
 
 function PriceCard({ stockSymbol }) {
     return (
@@ -66,34 +68,7 @@ function PriceCard({ stockSymbol }) {
                                 </Badge>
                             </div>
                         </Col>
-                        <Col sm={9}>
-                            <div
-                                className={
-                                    stockSymbol.currentValueChange > 0
-                                        ? "chart-index chart-up"
-                                        : "chart-index chart-down"
-                                }
-                            >
-                                {stockSymbol !== null &&
-                                    stockSymbol.symbol !== undefined && (
-                                        <ChartBoard
-                                            selectedStock={stockSymbol}
-                                            chartType="AreaChart"
-                                            showStockName={false}
-                                            range="1d"
-                                            updown={
-                                                stockSymbol.currentValueChange >
-                                                0
-                                                    ? 1
-                                                    : stockSymbol.currentValueChange ===
-                                                      0
-                                                    ? 0
-                                                    : -1
-                                            }
-                                        />
-                                    )}
-                            </div>
-                        </Col>
+                        
                     </Row>
                 </Container>
             </div>
